19db9cbed39e4f9c69901d289e4eccf6f60cb48f
Fahad Pathan <fahadpathan56@gmail.com>
2023-09-09 22:18:00 +0600

    add: main function
    1. printing something

--- main.go
+++ main.go
@@ -2,3 +2,7 @@ package main // package declaration. main is a special package name which tells 
 
 import "fmt" // import declaration. fmt is a standard library package which contains functions for printing formatted output and scanning input from the console.
 
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Print("I am learning go")
+}
