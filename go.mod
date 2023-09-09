4daa6163687c5ace33302c3f3795cfc5ba34d1bf
Fahad Pathan <fahadpathan56@gmail.com>
2023-09-09 21:34:01 +0600

    initialization

---
+++ go.mod
@@ -0,0 +1,3 @@
+module LEARNING-GO
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,4 @@
+package main // package declaration. main is a special package name which tells the Go compiler that the package should compile as an executable program instead of a shared library.
+
+import "fmt" // import declaration. fmt is a standard library package which contains functions for printing formatted output and scanning input from the console.
+
