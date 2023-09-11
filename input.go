602331f4eed510ee5710ef97155c3a85917f6d83
Fahad Pathan <fahadpathan56@gmail.com>
2023-09-11 21:00:10 +0600

    add: input
    1. scan
    2. scanln
    3. scanf

---
+++ input.go
@@ -0,0 +1,31 @@
+
+// 3 ways to get input from the user.
+// these are scan, scanln, and scanf
+// scan - reads a single word
+// scanln - reads a line
+// scanf - reads a formatted string
+
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	var age int
+
+	// using scan
+	fmt.Scan(&name, &age)
+
+	fmt.Printf("Name: %v\nAge: %v\n", name, age)
+
+	// // using scanln
+	// fmt.Scanln(&name)
+	// fmt.Scanln(&age)
+
+	// fmt.Printf("Name: %v\nAge: %v\n", name, age)
+
+	// // using scanf
+	// fmt.Scanf("%v %v", &name, &age)
+
+	// fmt.Printf("Name: %v\nAge: %v\n", name, age)
+}
