7a7dc2c4845d9351c501aadc73f68e656c462063
Fahad Pathan <fahadpathan56@gmail.com>
2023-09-14 11:08:49 +0600

    update: switch
    1. range switch

--- switch.go
+++ switch.go
@@ -16,4 +16,17 @@ func main() {
 	default:
 		fmt.Printf("a = %d\n", a)
 	}
+
+
+	// range switch
+	switch {
+	case a >= 0 && a <= 50:
+		fmt.Printf("a >= 0 && a <= 50\n")
+		break
+	case a >= 51 && a <= 100:
+		fmt.Printf("a >= 51 && a <= 100\n")
+		break
+	default:
+		fmt.Printf("a = %d\n", a)
+	}
 }
